86d8b73db0a1b6c0186d0dc0f52fae6f982e5087
Farhan Tariq <farhantariq1016@gmail.com>
2025-10-23 13:46:17 +0500

    Update SAI/Makefile and dash_run.sh
    Signed-off-by: Farhan Tariq <farhantariq1016@gmail.com>

--- dash-pipeline/dash_run.sh
+++ dash-pipeline/dash_run.sh
@@ -1,3 +1,4 @@
+
 #To generate all the python model artifacts, run:
 make py-artifacts
 
@@ -10,3 +11,33 @@ make pymodel
 #To run the python model with DPAPP support, run:
 make pymodel HAVE_DPAPP=y
 
+#To build SAI for the python model, run:
+sudo make sai TARGET_MODEL=pymodel
+
+#To build SAI for the bmv2, run:
+sudo make sai
+
+#To clean SAI headers, run:
+sudo make sai-clean HOST_USER=$(id -u) HOST_GROUP=$(id -g)
+
+#To build SAI Server, run:
+sudo chown -R farhan:farhan .
+sudo make saithrift-server HOST_USER=$(id -u) HOST_GROUP=$(id -g)
+
+#To clean SAI Server, run:
+sudo make saithrift-server-clean HOST_USER=$(id -u) HOST_GROUP=$(id -g)
+
+#To build SAI Client, run:
+make docker-saithrift-client
+
+#To run DPAPP, run:
+make run-dpapp
+
+#To run SAI Server, run:
+# make saithrift-server
+docker rm -f dash-saithrift-server-"${USER}"
+make run-saithrift-server
+
+#To run PTF Tests, run:
+make docker-saithrift-client
+make run-saithrift-ptftests
